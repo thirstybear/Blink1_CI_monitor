78310d073c322552b5dcc287dcee7f9168230204
Chris Pitts <csp@skycdc.com>
2016-06-09 17:28:43 +0100

    Don't bother flashing or setting colour unless there is a state change.

--- src/main/java/uk/co/thirstybear/blink1jenkins/blink1/Blink1Worker.java
+++ src/main/java/uk/co/thirstybear/blink1jenkins/blink1/Blink1Worker.java
@@ -1,20 +1,33 @@
 package uk.co.thirstybear.blink1jenkins.blink1;
 
+import uk.co.thirstybear.blink1jenkins.jenkins.JenkinsState;
+
 import static uk.co.thirstybear.blink1jenkins.blink1.actions.Blink1Action.*;
+import static uk.co.thirstybear.blink1jenkins.jenkins.JenkinsState.FAIL;
+import static uk.co.thirstybear.blink1jenkins.jenkins.JenkinsState.PASS;
 
 public class Blink1Worker {
+    private JenkinsState currentState;
 
     public void buildFailed() {
+        if (currentState == FAIL) return;
+
+        currentState = FAIL;
         FLASHRED.apply();
         SOLIDRED.apply();
     }
 
     public void buildPassed() {
+        if (currentState == PASS) return;
+
+        currentState = PASS;
         FLASHGREEN.apply();
         SOLIDGREEN.apply();
     }
 
     public void oops() {
+        currentState = null;
         FLASHRED_CONTINUOUS.apply();
     }
+
 }
